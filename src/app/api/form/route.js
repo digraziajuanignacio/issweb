60dbf0c6a18525af3a94e129b1e72ca44ffb41fe
digraziajuanignacio <nachitodalo@gmail.com>
2024-11-06 16:01:35 -0300

    cors

--- src/app/api/form/route.js
+++ src/app/api/form/route.js
@@ -2,13 +2,37 @@ import connectDB from "@/app/lib/mongodb";
 import Contact from "@/app/models/form";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET", "POST"],
+  origin: "https://issweb.onrender.com",
+});
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export async function POST(req) {
-  const { fullname, email, dniAlumno, dniRespo, solicitudVacante } = await req.json();
+  await runMiddleware(req, res, cors);
+  const { fullname, email, dniAlumno, dniRespo, solicitudVacante } =
+    await req.json();
 
   try {
     await connectDB();
-    await Contact.create({ fullname, email, dniAlumno, dniRespo, solicitudVacante });
+    await Contact.create({
+      fullname,
+      email,
+      dniAlumno,
+      dniRespo,
+      solicitudVacante,
+    });
 
     return NextResponse.json({
       msg: ["Inscripción enviada correctamente."],
@@ -23,7 +47,9 @@ export async function POST(req) {
       console.log(errorList);
       return NextResponse.json({ msg: errorList });
     } else {
-      return NextResponse.json({ msg: ["La inscripción no se ha podido enviar correctamente."] });
+      return NextResponse.json({
+        msg: ["La inscripción no se ha podido enviar correctamente."],
+      });
     }
   }
 }
